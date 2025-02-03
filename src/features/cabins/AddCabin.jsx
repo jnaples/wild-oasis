e16cebe6281e2acba91a56dec83a02c67e29faf6
dojo <jnaples90@gmail.com>
2025-02-03 15:29:14 -0500

    Refactor modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
-export default function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
+function AddCabin() {
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onCloseModal={() => setIsOpenModal(false)}>
-          <CreateCabinForm
-            onCloseModal={() => setIsOpenModal((show) => !show)}
-          />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+export default AddCabin;
