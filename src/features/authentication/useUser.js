25205cb9a8aac04677a1079e1bc0fcd8fc8f733f
dojo <jnaples90@gmail.com>
2025-02-27 06:48:51 -0500

    Add login authentication

---
+++ src/features/authentication/useUser.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "../../services/apiAuth";
+
+export function useUser() {
+  const {
+    isLoading,
+    data: user,
+    fetchStatus,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: getCurrentUser,
+  });
+
+  return {
+    isLoading,
+    user,
+    fetchStatus,
+    isAuthenticated: user?.role === "authenticated",
+  };
+}
