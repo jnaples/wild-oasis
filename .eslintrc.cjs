2e3226cb7d21cd7187466c8fe2591001860e34e6
dojo <jnaples90@gmail.com>
2025-03-14 13:21:41 -0400

    Add filter to dashboard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
     ],
     "no-unused-vars": "warn",
     "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
     "react-refresh/only-export-components": [
       "off",
       { allowConstantExport: true },
